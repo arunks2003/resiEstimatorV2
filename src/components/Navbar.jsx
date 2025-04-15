6e92c967c0f9cc304c805832008cc2552819e5d0
Arun Kumar <arun.kumar.cd.civ21@itbhu.ac.in>
2025-04-16 00:19:25 +0530

    again fixed navbar-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -117,7 +117,7 @@ const Navbar = ({ setStep, setFlag }) => {
                     </Link>
                     <Link
                         to="/methodology"
-                        className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                        className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
                     >
                         Methodology
                     </Link>
